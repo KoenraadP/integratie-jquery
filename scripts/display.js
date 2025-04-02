21501f32df09695cd091b93ec3bd176111576a43
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-04-02 09:44:43 +0200

    944

---
+++ scripts/display.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// alles binnen $(function()) wordt pas uitgevoerd
+// nadat de browser de volledige HTML code verwerkt heeft
+// het dollar-teken is de verkorte versie van het woord jQuery
+$(function(){
+
+    $("#redbox").fad
+    
+})
